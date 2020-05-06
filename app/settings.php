dcd2e49f44b409f8148a7fddc44ed933df79ab66
yokenzan <31175068+yokenzan@users.noreply.github.com>
2020-05-07 00:02:10 +0900

    update settings to refer .env config parameters

--- app/settings.php
+++ app/settings.php
@@ -1,18 +1,28 @@
 <?php
+
 declare(strict_types=1);
 
 use DI\ContainerBuilder;
+use Dotenv\Dotenv;
 use Monolog\Logger;
 
-return function (ContainerBuilder $containerBuilder) {
+return function (ContainerBuilder $containerBuilder): void {
+    Dotenv::createImmutable(__DIR__ . ' /..')->load();
+
     // Global Settings Object
     $containerBuilder->addDefinitions([
         'settings' => [
-            'displayErrorDetails' => true, // Should be set to false in production
+            'displayErrorDetails' => getenv('APP_ENV') !== 'production', // Should be set to false in production
             'logger' => [
-                'name' => 'slim-app',
+                'name' => getenv('APP_NAME') ?? 'Zhinahan',
                 'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
                 'level' => Logger::DEBUG,
+            ],
+            'db' => [
+                'connection' => getenv('DB_CONNECTION'),
+                'dbname'     => getenv('DB_DATABASE'),
+                'user'       => getenv('DB_USERNAME'),
+                'pass'       => getenv('DB_PASSWORD'),
             ],
         ],
     ]);
